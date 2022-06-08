6f449fca286b6567f4e69819b3451f91a4220519
unknown <misterjimlee@gmail.com>
2022-06-08 17:31:18 -0400

    ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,13 @@ var taskFormHandler = function() {
     var taskNameInput = document.querySelector("input[name='task-name'").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
